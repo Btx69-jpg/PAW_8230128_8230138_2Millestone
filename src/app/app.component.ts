ec4f8b4f07fcaa7423327767727d74c0002bcab3
Btx69-jpg <8230127@estg.ipp.pt>
2025-04-30 09:42:25 +0100

    start project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'PAW_8230128_8230138_2Millestone';
+}
