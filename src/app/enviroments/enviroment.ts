eac3656be8e3e477af283791592cda13fd9f030c
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-07-03 16:21:41 +0100

    Delete enviroment.ts

--- src/app/enviroments/enviroment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000',
-    stripePublicKey: 'pk_test_51RSMp1H5rbaEKCF8sVLyeoTZOvia7PsepJAFYFMkriWOKqWkrgpAfJjsI2qOxm8K8PkpeczXDVxmegczf0rFSCLB00kGCiYpv3'
-};
